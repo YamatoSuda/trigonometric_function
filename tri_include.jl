0d55fdd77562e3df23ac3be1ebd7483234968668
moruten <vgwy7814@yahoo.co.jp>
2022-01-03 09:18:45 +0900

    2022-1-3 edit

--- tri_include.jl
+++ tri_include.jl
@@ -1,5 +1,40 @@
-using Plots
-using Pkg
-Pkg.add("Gtk")
-
 include("tri_functions.jl")
+
+win = GtkWindow("Trigonometric Funcion Plot Program", 1000, 800) 
+#======================================================================================#
+#winは多くの引数を持つオブジェクト．引数のことを，"property"と呼ぶ． 
+#set_gtk_property!(win, :title, "New title") のように書くことで，propertyの変更が可能．
+# example) win = GtkWindow("winの名前", 横サイズ, 縦サイズ)
+#======================================================================================#
+
+#=
+#= Code1 =#
+hbox    = GtkBox(:h)  # :h makes a horizontal layout, :v a vertical layout
+push!(win, hbox)
+cancel  = GtkButton("Cancel")
+ok      = GtkButton("OK")
+push!(hbox, cancel)
+push!(hbox, ok)
+showall(win)
+=#
+#=========================================================================================#
+#Gtkでの構成は，"オブジェクトの作成" -> "winへのpush" という流れでおこなう．
+#上記の例では，GtkBox(:h)で横方向の箱を用意し，winにpush,
+#cansel,okのボタンを GtkButton("~")で用意し，hboxの中へpush
+#というステップをふんでいる．
+#=========================================================================================#
+
+#= Code2 =#
+hbox   = GtkButtonBox(:h)
+ok     = GtkButton("OK")
+cancel = GtkButton("Cancel")
+push!(win, hbox)
+push!(hbox, cancel)
+push!(hbox, ok)
+showall(win)
+#=========================================================================================#
+#Code1の例では，okとcancelの箱の大きさがあまりキレイじゃない．
+#そこで，GtkButtonBoxが用意されている．これは，文字列の長さに応じて箱の大きさを変えてくれる関数となっている．
+#まず，hbox=GtkButtonBox(:h) [:vは垂直方向のbox]で箱を用意し，winにpushする．
+#次に，ボタンを用意し，用意したhboxにボタンをpushする．
+#=========================================================================================#
--- trigonometric_function_body.jl
+++ trigonometric_function_body.jl
@@ -1,33 +1,4 @@
 #= https://juliagraphics.github.io/Gtk.jl/latest/manual/signals/ =#
+using Plots
 using Gtk
-include("tri_functions.jl")
-
-win = GtkWindow("Trigonometric funcion plot Program", 1000, 800)
-
-#b = GtkButton("Click Me")
-#push!(win,b)
-hbox = GtkBox(:h)  # :h makes a horizontal layout, :v a vertical layout
-push!(win, hbox)
-cancel = GtkButton("Cancel")
-ok = GtkButton("OK")
-push!(hbox, cancel)
-push!(hbox, ok)
-showall(win)
-#signal_connect(on_button_clicked, b, "clicked");
-#showall(win);
-length(hbox)
-get_gtk_property(hbox[1], :label, String)
-get_gtk_property(hbox[2], :label, String)
-
-set_gtk_property!(hbox,:expand,ok,true)
-set_gtk_property!(hbox,:spacing,10)
-
-
-
-
-
-
-
-
-
-
+include("tri_include.jl")
